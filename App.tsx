1ca7c2cfbe84041c833fbc49373677bcd3f507a2
tanimpersonal <tashintanim2019@gmail.com>
2024-03-04 20:04:55 +0600

    hello-world-complete

--- App.tsx
+++ App.tsx
@@ -1,10 +1,38 @@
 import React from 'react';
-import {View, Text, SafeAreaView} from 'react-native';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  useColorScheme,
+} from 'react-native';
+const customStyle = StyleSheet.create({
+  container: {
+    backgroundColor: 'red',
+  },
+  containerWhite: {
+    backgroundColor: 'white',
+  },
+  viewStyle: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    // gap: '20px',
+  },
+});
 function App() {
+  const deviceStyle = useColorScheme();
+
   return (
-    <SafeAreaView>
-      <View>
+    <SafeAreaView
+      style={
+        deviceStyle == 'light'
+          ? customStyle.containerWhite
+          : customStyle.container
+      }>
+      <View style={customStyle.viewStyle}>
         <Text>Hello World</Text>
+        <Text>{deviceStyle}</Text>
       </View>
     </SafeAreaView>
   );
